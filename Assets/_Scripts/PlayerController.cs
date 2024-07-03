127fce26c7a84da2348f24d2767ef91af6157b2d
Dophian <169108628+Dophian@users.noreply.github.com>
2024-07-03 18:56:41 +0900

    더블탭 스크립트 시작

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,71 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rbody;
-    float axisH = 0.0f;
-    public float speed = 1.0f;
-    public float sprint = 3.0f;
+    // 따닥
+    private struct Tapinfo
+    {
+        public Tapinfo(KeyCode keyCode, float tapTime)
+        {
+            this.keyCode = keyCode;
+            this.tapTime = tapTime;
+            onDoubleTap = null;
+        }
+
+        public KeyCode keyCode;
+        public float tapTime;
+        public Action onDoubleTap;
+    }
+    private Tapinfo[] _tapInfos =
+        { 
+        new Tapinfo(KeyCode.A, 0f),
+        new Tapinfo(KeyCode.D, 0f)
+        };
+
+    private float _doubleTapTime = 0.5f;
+
+
+    Rigidbody2D rigidbody;
+    public float speed;
+    public float defaultspeed;
+
 
     public float jump = 3.0f;
     public LayerMask grondLayer;
     bool goJump = false;
     bool onGround = false;
 
-    // 따닥
-    int buttonPresses = 0;
-    float buttonPressTimer = 0.5f;
-
-    // 애니메이션
-    Animator animator;
-    public string stopAnime = "KirbyIdle";
-    public string walkAnime = "KirbyWalk";
-    public string jumpAnime = "KirbyJump";
-    string nowAnime = "";
-    string oldAnime = "";
-
     private void Start()
     {
-        rbody = this.GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        nowAnime = stopAnime;
-        oldAnime = stopAnime;
+        defaultspeed = speed;
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        _tapInfos[0].onDoubleTap += () => Debug.Log("W 키 더블탭");
+        _tapInfos[1].onDoubleTap += () => Debug.Log("D 키 더블탭");
     }
 
     private void Update()
     {
-        axisH = Input.GetAxisRaw("Horizontal");
+        for (int i = 0; i < _tapInfos.Length; i++)
+        {
+            if (Input.GetKeyDown(_tapInfos[i].keyCode))
+            {
+                float elapsedTime = Time.time - _tapInfos[i].tapTime;
 
-        if (axisH > 0.0f)
-        {
-            Debug.Log("오른쪽 이동");
-            transform.localScale = new Vector2(1, 1);
+                if (elapsedTime <= _doubleTapTime)
+                    defaultspeed = speed * 3;
+
+                _tapInfos[i].tapTime = Time.time;
+            }
         }
-        else if (axisH < 0.0f)
-        {
-            Debug.Log("왼쪽 이동");
-            transform.localScale = new Vector2(-1, 1);
-        }
+
+        float hor = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(hor * defaultspeed , rigidbody.velocity.y);
+
+        //if(Input.GetKey(KeyCode.LeftShift))
+        //{
+        //    defaultspeed = speed * 3;
+        //}
+        //else
+        //{
+        //    defaultspeed = speed;
+        //}
 
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
         }
-
-        if (axisH == 0)
-        {
-            rbody.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-        }
-        else
-        {
-            rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
-        }
-
-        // 따닥
-
-
     }
 
     private void FixedUpdate()
@@ -74,44 +86,15 @@ public class PlayerController : MonoBehaviour
         // onGround가 true로 설정돼어야 지면에 있다고 판단해서 걸어다님.
         onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.01f), grondLayer);
 
-        if (onGround || axisH != 0)
-        {
-            rbody.velocity = new Vector2(speed * axisH, rbody.velocity.y);
-        }
 
         if (onGround && goJump)
         {
             Debug.Log("점프!");
             Vector2 jumpPw = new Vector2(0, jump);
-            rbody.AddForce(jumpPw, ForceMode2D.Impulse);
+            rigidbody.AddForce(jumpPw, ForceMode2D.Impulse);
             goJump = false;
         }
 
-        if (onGround)
-        {
-            if (axisH == 0)
-            {
-                nowAnime = stopAnime;
-            }
-            else
-            {
-                nowAnime = walkAnime;
-            }
-        }
-
-        else
-        {
-            nowAnime = jumpAnime;
-        }
-
-        if (nowAnime != oldAnime)
-        {
-            Debug.Log(nowAnime);
-            Debug.Log(oldAnime);
-
-            oldAnime = nowAnime;
-            animator.Play(nowAnime);
-        }
     }
 
     public void Jump()
